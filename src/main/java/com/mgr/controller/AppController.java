5f2de1584a5d5831caaa824a14825fb5d0b9e0a0
ramachandra.m <ramachandra.m@xcubelabs.com>
2023-11-19 00:42:25 +0530

    adding user controller

--- src/main/java/com/mgr/controller/AppController.java
+++ src/main/java/com/mgr/controller/AppController.java
@@ -1,15 +1,22 @@
 package com.mgr.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/mgr")
+@RequestMapping("/api")
 public class AppController {
 
 	@GetMapping("/")
 	private String information() {
-		return "MGR app 0.0.1";
+		return "Hi Hello welcome To MGR app 0.0.1";
+	}
+	
+	@GetMapping("/contact")
+	private String hello() {
+		return "M.Ramachandra,mgr@gmail.com,7093419328";
 	}
 }
---
+++ src/main/java/com/mgr/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.mgr.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+	
+	@PostMapping("/{name}")
+	private String saveUser(@PathVariable String name) {
+		return name;
+	}
+}
